759205d6a28d76cb4b8f7accc8bf90667deea551
phamuoc <148070737+phamuoc@users.noreply.github.com>
2024-01-09 00:27:34 +0700

    Add files via upload

---
+++ CHUONG 12/bài tập chương 12.1.py
@@ -0,0 +1,79 @@
+#c8 b1
+from ham_chuong8 import chuong8_bai1
+chuong8_bai1()
+
+#c8 b2
+from ham_chuong8 import chuong8_bai2
+chuong8_bai2()
+
+#c8 b3
+from ham_chuong8 import chuong8_bai3
+chuong8_bai3()
+
+#c8 b4
+from ham_chuong8 import chuong8_bai4
+chuong8_bai4()
+
+#c8 b5
+from ham_chuong8 import chuong8_bai5
+chuong8_bai5()
+
+#c8 b6
+from ham_chuong8 import chuong8_bai6
+chuong8_bai6()
+
+#c8 b7
+from ham_chuong8 import chuong8_bai7
+chuong8_bai7()
+
+#c8 b8
+from ham_chuong8 import chuong8_bai8
+chuong8_bai8()
+
+#c8 b9 
+from ham_chuong8 import chuong8_bai9
+chuong8_bai9()
+
+#c8 b10
+from ham_chuong8 import chuong8_bai10
+chuong8_bai10()
+
+#c8 b11
+from ham_chuong8 import chuong8_bai11
+chuong8_bai11()
+
+#c8 b12
+from ham_chuong8 import chuong8_bai12
+chuong8_bai12()
+
+#c8 b13
+from ham_chuong8 import chuong8_bai13
+chuong8_bai13()
+
+#c8 b14
+from ham_chuong8 import chuong8_bai14
+chuong8_bai14()
+
+#c8 b15
+from ham_chuong8 import chuong8_bai15
+chuong8_bai15()
+
+#c8 b16
+from ham_chuong8 import chuong8_bai16
+chuong8_bai16()
+
+#c8 b17
+from ham_chuong8 import chuong8_bai17
+chuong8_bai17()
+
+#c8 b18
+from ham_chuong8 import chuong8_bai18
+chuong8_bai18()
+
+#c8 b19
+from ham_chuong8 import chuong8_bai19
+chuong8_bai19()
+
+#c8 b20
+from ham_chuong8 import chuong8_bai20
+chuong8_bai20()
---
+++ CHUONG 12/bài tập chương 12.2.py
@@ -0,0 +1,43 @@
+#c11 b1
+from ham_chuong11 import chuong11_bai1
+chuong11_bai1()
+
+#c11 b2
+from ham_chuong11 import chuong11_bai2
+chuong11_bai2()
+
+#c11 b3
+from ham_chuong11 import chuong11_bai3
+chuong11_bai3()
+
+#c11 b4
+from ham_chuong11 import chuong11_bai4
+chuong11_bai4()
+
+#c11 b5
+from ham_chuong11 import chuong11_bai5
+chuong11_bai5()
+
+#c11 b6
+from ham_chuong11 import chuong11_bai6
+chuong11_bai6()
+
+#c11 b7
+from ham_chuong11 import chuong11_bai7
+chuong11_bai7()
+
+#c11 b8
+from ham_chuong11 import chuong11_bai8
+chuong11_bai8()
+
+#c11 b9
+from ham_chuong11 import chuong11_bai9
+chuong11_bai9()
+
+#c11 b11
+from ham_chuong11 import chuong11_bai11
+chuong11_bai11()
+
+#c11 b12
+from ham_chuong11 import chuong11_bai12
+chuong11_bai12()
